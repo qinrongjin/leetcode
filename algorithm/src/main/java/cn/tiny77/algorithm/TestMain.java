bffdb1bda58c65cac4057471072d3c98cdae81fc
qinrongjin <qinrongjin@126.com>
2018-02-08 15:48:27 +0800

    all

--- algorithm/src/main/java/cn/tiny77/algorithm/TestMain.java
+++ algorithm/src/main/java/cn/tiny77/algorithm/TestMain.java
@@ -6,11 +6,12 @@ import java.util.regex.Pattern;
 public class TestMain {
 
 	public static void main(String[] args) {
-		Pattern pattern = Pattern.compile("ab?");
-		Matcher matcher = pattern.matcher("mmassaab");
-		System.out.println(matcher.find());
-		System.out.println(matcher.group());
-		System.out.println(matcher.group());
+		String as = "dsd";
+		t(23);
 	}
 
+	
+	static <U> void t(U u) {
+		System.out.println(u);
+	}
 }
