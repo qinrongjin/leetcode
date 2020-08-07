6e17ac787d033068efc4e86fc2edb07473655171
覃荣锦 <qinrongjin@kanzhun.com>
2020-08-07 21:58:48 +0800

    提交

--- algorithm/src/main/java/cn/tiny77/algorithm/from0000to0100/n044/Solution.java
+++ algorithm/src/main/java/cn/tiny77/algorithm/from0000to0100/n044/Solution.java
@@ -3,52 +3,57 @@ package cn.tiny77.algorithm.from0000to0100.n044;
 class Solution {
 
     public static void main(String[] args) {
-        System.out.println(new Solution().isMatch("bbbbbbbabbaabbabbbbaaabbabbabaaabbababbbabbbabaaabaab", "b*b*ab**ba*b**b***bba"));
-        System.out.println(new Solution().isMatch("abbabaaabbabbaababbabbbbbabbbabbbabaaaaababababbbabababaabbababaabbbbbbaaaabababbbaabbbbaabbbbababababbaabbaababaabbbababababbbbaaabbbbbabaaaabbababbbbaababaabbababbbbbababbbabaaaaaaaabbbbbaabaaababaaaabb", "**aa*****ba*a*bb**aa*ab****a*aaaaaa***a*aaaa**bbabb*b*b**aaaaaaaaa*a********ba*bbb***a*ba*bb*bb**a*b*bb"));
-        System.out.println(new Solution().isMatch("a", "a**"));
-        System.out.println(new Solution().isMatch("acdcb", "a*c?b"));
+        System.out.println(new Solution().isMatch("adceb", "*a*b"));
     }
 
     public boolean isMatch(String s, String p) {
-        return isMatch(s, 0, p, 0);
+        boolean[][] match = new boolean[s.length() + 1][p.length() + 1];
+        match[0][0] = true;
+        boolean flag = true;
+        for (int j = 1; j <= p.length(); j++) {
+            char pc = p.charAt(j - 1);
+            if (pc != '*') {
+                flag = false;
+            }
+            for(int i = 1; i <= s.length() ; i ++) {
+                char sc = s.charAt(i - 1);
+                if (flag) {
+                    match[i][j] = true;
+                } else {
+                    if (pc == '*') {
+                        match[i][j] = match[i - 1][j] || match[i][j - 1];
+                    } else {
+                        if (pc == '?' || pc == sc) {
+                            match[i][j] = match[i - 1][j - 1];
+                        }
+                    }
+                }
+            }
+        }
+        displayMatch(match);
+        return match[s.length()][p.length()];
     }
 
-    private boolean isMatch(String s, int si, String p, int pi) {
-        if (si == s.length() && pi == p.length()) {
-            return true;
-        }
-        if (p.length() > pi) {
-            char pc = p.charAt(pi);
-            if (pc != '*') {
-                if (si < s.length() && (pc == '?' ||  pc == s.charAt(si))) {
-                    return isMatch(s, si + 1, p, pi + 1);
+    private void displayMatch(boolean[][] match) {
+        for (int i = -1; i < match.length; i ++) {
+            for (int j = -1; j < match[0].length; j ++) {
+                if (i == -1) {
+                    if (j == -1) {
+                        System.out.print(" \t");
+                    } else {
+                        System.out.print(" " + j + "\t");
+                    }
                 } else {
-                    return false;
-                }
-            } else {
-                while (true) {
-                    if (pi == p.length() - 1) {
-                        return true;
-                    }
-                    if (p.charAt(pi) != p.charAt(pi + 1)) {
-                        break;
-                    }
-                    pi ++;
-                }
-                for (; si <= s.length(); si ++) {
-                    boolean result = isMatch(s, si, p, pi + 1);
-                    if (result) {
-                        return true;
+                    if (j == -1) {
+                        System.out.print(" " + i + "\t");
+                    } else {
+                        System.out.print("[" + (match[i][j] ? 1 : 0) + "]\t");
                     }
                 }
-                return false;
             }
-        } else {
-            if (s.length() > si) {
-                return false;
-            } else {
-                return true;
-            }
+            System.out.println();
         }
     }
+
+
 }
